0295265ae493899e565be3b6dd60d3a10f906955
carreguz13 <carreguz13@gmail.com>
2023-09-06 14:44:23 -0700

    homepage done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
-function Footer({ footerBanner }) {
-  return <div>Footer</div>;
+function Footer() {
+  return (
+    <div className="footer-container">
+      <p>2023 STR Headphones All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
